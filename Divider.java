126eaf60818dc9b693c8ab6dc4bdab41a4597662
christinasoto <63577342+christinasoto@users.noreply.github.com>
2020-04-18 17:06:33 -0500

    Update Divider.java

--- Divider.java
+++ Divider.java
@@ -1,3 +1,12 @@
+/**
+ * Allows implementation for divide method
+ *
+ * JDK version: 8.0.0
+ *
+ * @author Christina Soto
+ *  * @version 8.0.0
+ *  * @since 04-18-2020
+ */
 public interface Divider{
     void divide(int y);
 }
