eec63c5d9fbe3a2c97d9b2f51521a6a0222fb734
christinasoto <63577342+christinasoto@users.noreply.github.com>
2020-04-18 16:47:36 -0500

    Add files via upload

---
+++ UnitTests.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class UnitTests {
+    //need unit tests for multiply, divide and calculate
+    public static void main(String args[]) {
+
+        MiniCalculator test = new MiniCalculator();
+
+        test.multiply(10);
+        if (test.calculate() != 10)
+            System.out.println("\n FAILED TO MULTIPLY");
+        else System.out.println("\n SUCCESSFULLY MULTIPLIED");
+
+        test.divide(5);
+        if (test.calculate() != 2)
+            System.out.println("\n FAILED TO DIVIDE");
+        else
+            System.out.println("\n SUCCESSFULLY DIVIDED");
+
+        test.calculate();
+        if(test.calculate() != 0)
+        System.out.println("\n OPERATIONS CORRECTLY CALCULATED");
+        else
+            System.out.println("\n OPERATIONS NOT CORRECTLY CALCULATED");
+
+    }
+}
+
