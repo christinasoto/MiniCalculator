90228b78920d3114622896141ac3fde84bdbfcd0
christinasoto <63577342+christinasoto@users.noreply.github.com>
2020-04-18 17:07:57 -0500

    Update MiniCalculator.java

--- MiniCalculator.java
+++ MiniCalculator.java
@@ -1,8 +1,15 @@
+/**
+ * Inherits Calculator and returns the result.
+ *
+ * JDK version: 8.0.0
+ *
+ * @author Christina Soto
+ *  * @version 8.0.0
+ *  * @since 04-18-2020
+ */
 
 public class MiniCalculator extends Calculator{
-
-
-
+    
     public int calculate() {
         return total;
     }
